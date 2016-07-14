e4029aabef86ff7d35f92a310f09201ff1447279
Rafael Piazdecki <rafael.piazdecki@b2net.pl>
2016-07-15 00:21:26 +0200

    survivr main code passes

--- lib/survivr.rb
+++ lib/survivr.rb
@@ -5,16 +5,16 @@ require_relative "jury"
 
 #After your tests pass, uncomment this code below
 #=========================================================
-# # Create an array of twenty hopefuls to compete on the island of Borneo
-# @contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
-# @contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
-#
-# # Create two new tribes with names
-# @coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
-# @hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
-#
-# # Create a new game of Survivor
-# @borneo = Game.new(@coyopa, @hunapu)
+# Create an array of twenty hopefuls to compete on the island of Borneo
+@contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
+@contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
+
+# Create two new tribes with names
+@coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
+@hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
+
+# Create a new game of Survivor
+@borneo = Game.new(@coyopa, @hunapu)
 #=========================================================
 
 
@@ -22,7 +22,9 @@ require_relative "jury"
 def phase_one
   
   8.times do |turn|
-
+    losers_tribe = @borneo.immunity_challenge
+    loser = losers_tribe.tribal_council
+    losers_tribe.members.delete loser
   end
   
 end
@@ -30,7 +32,9 @@ end
 def phase_two
   
   3.times do |turn|
-
+    immune_contestant = @borneo.individual_immunity_challenge
+    loser = @merge_tribe.tribal_council immune: immune_contestant
+    @merge_tribe.members.delete loser
   end
   
 end
@@ -38,7 +42,10 @@ end
 def phase_three
   
   7.times do |turn|
-
+    immune_contestant = @borneo.individual_immunity_challenge
+    loser = @merge_tribe.tribal_council immune: immune_contestant
+    @merge_tribe.members.delete loser
+    @jury.add_member loser
   end
   
 end
@@ -46,12 +53,12 @@ end
 
 # If all the tests pass, the code below should run the entire simulation!!
 #=========================================================
-# phase_one #8 eliminations
-# @merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
-# phase_two #3 more eliminations
-# @jury = Jury.new
-# phase_three #7 elminiations become jury members
-# finalists = @merge_tribe.members #set finalists
-# vote_results = @jury.cast_votes(finalists) #Jury members report votes
-# @jury.report_votes(vote_results) #Jury announces their votes
-# @jury.announce_winner(vote_results) #Jury announces final winner
+phase_one #8 eliminations
+@merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
+phase_two #3 more eliminations
+@jury = Jury.new
+phase_three #7 elminiations become jury members
+finalists = @merge_tribe.members #set finalists
+vote_results = @jury.cast_votes(finalists) #Jury members report votes
+@jury.report_votes(vote_results) #Jury announces their votes
+@jury.announce_winner(vote_results) #Jury announces final winner
