493d0c36abb06859475be936845c7592b59033f3
Rafael Piazdecki <rafael.piazdecki@b2net.pl>
2016-07-14 23:05:55 +0200

    tribe tests passes

---
+++ lib/tribe.rb
@@ -0,0 +1,18 @@
+class Tribe
+  attr_reader :name, :members
+  
+  def initialize options = {}
+    @name = options[:name]
+    @members = options[:members]
+    puts @members
+  end
+  
+  def tribal_council options={}
+    @members.reject{ |member| member == options[:immune]}.sample
+  end
+  
+  def to_s
+    @name
+  end
+  
+end
